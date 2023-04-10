75b3ce30144b0d44e140a9904faafdb880dd1bf8
pidjieudibril <pidjieudibril@gmail.com>
2023-04-10 02:58:17 -0300

    premier partie du projet termine

--- data/script.js
+++ data/script.js
@@ -1,4 +1,4 @@
-/*
+// obtenir les valeur de temperature apres chaque deux seconde 
 setInterval(function ()
 {
     var xhttp = new XMLHttpRequest();
@@ -16,27 +16,7 @@ setInterval(function ()
    
     xhttp.send();
 }, 2000);
-
-
-setInterval(function ()
-{
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function()
-    {
-        if(this.readyState == 4 && this.status == 200)
-        {
-         
-            document.getElementById("humidity").innerHTML = this.responseText;
-           
-        }
-    };
-
-   
-    xhttp.open("GET", "/lireHumidity", true);
-    
-    xhttp.send();
-}, 2000);
+//lecture de la presion en utilisant ajax apres chaque deux seconde
 setInterval(function ()
 {
     var xhttp = new XMLHttpRequest();
@@ -49,11 +29,11 @@ setInterval(function ()
             document.getElementById("presure").innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "/lirePresure", true);;
+    xhttp.open("GET", "/LirePresure", true);;
    
     xhttp.send();
 }, 2000);
-
+// lecture de l'altitude apres chaque deux seconde 
 setInterval(function ()
 {
     var xhttp = new XMLHttpRequest();
@@ -69,27 +49,31 @@ setInterval(function ()
     xhttp.send();
 }, 2000);
 
-*/
+
 
 
 
 // Function to get current readings on the webpage when it loads for the first time
-
-function getReadings() {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var myObj = JSON.parse(this.responseText);
-      console.log(myObj);
-      document.getElementById("temperature").innerHTML = myObj.temperature;
-      document.getElementById("humidity").innerHTML = myObj.humidity;
-      document.getElementById("presure").innerHTML = myObj.pressure;
-      document.getElementById("altitude").innerHTML = myObj.altitude;
-      
-    }
-  };
-  xhr.open("GET", "/readings", true);
-  xhr.send();
-}
+function getReadings(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        console.log(myObj);
+        var temp = myObj.temperature;
+        var pres = myObj.presure;
+        var alt = myObj.altitude;
+        temperature.value = temp;
+        presure.value = pres;
+        altitude.value = alt;
+        
+      }
+    }; 
+    xhr.open("GET", "/readings", true);
+    xhr.send();
+  }
+  
+  
+  
 setInterval(getReadings,2000)
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,29 @@
 
 //---------declaration pour notre libraire-----//
 #include <Arduino.h>
-#include <Wire.h>             // Inclus la bibliothèque Wire.h, afin de pouvoir envoyer des instructions sur le port i2c
+#include <Wire.h> // Inclus la bibliothèque Wire.h, afin de pouvoir envoyer des instructions sur le port i2c
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_BMP280.h>
- #include <Arduino_JSON.h>                   
+#include <Arduino_JSON.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
+#include <AsyncTCP.h>
 
 // Create an Event Source on /events
 
 Adafruit_BMP280 bmp; // I2C
 
-
-//Variables to save values from HTML form
+// Variables to save values from HTML form
 String data;
 
-//Variable pour stocker les valeur de lecture 
+unsigned long previous_time = 0;
+unsigned long Delay = 2000;
+// Variable pour stocker les valeur de lecture
 float temp;
 float hum;
 float pres;
 float alt;
-
 
 // Json Variable to Hold Sensor Readings
 JSONVar readings;
@@ -31,26 +32,27 @@ JSONVar readings;
 // parametre wifi
 const char *ssid = "dibfanappart2";
 const char *password = "autorisation2";
-//const char *ssid = "UNIFI_IDO1";
-//const char *password = "42Bidules!";
-// serveur asynchrone 
+// const char *ssid = "UNIFI_IDO1";
+// const char *password = "42Bidules!";
+//  serveur asynchrone
 
 AsyncWebServer server(80);
 // Instanciation de la librairie BME280
 
-
-
 //-----------------FUNCTIONS TO HANDLE SENSOR READINGS-----------------//
 
 // Init BME280
-void initBMP(){
-   if (!bmp.begin(0x76)) {
+void initBMP()
+{
+  if (!bmp.begin(0x76))
+  {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    while (1) delay(10);
+                     "try a different address!"));
+    while (1)
+      delay(10);
   }
 
-   bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
+  bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
@@ -58,18 +60,18 @@ void initBMP(){
 }
 
 // Get Sensor Readings
-void getSensorReadings(){
-  
-  temp =bmp.readTemperature();
-  pres= bmp.readPressure()/100.0F;
+void getSensorReadings()
+{
+
+  temp = bmp.readTemperature();
+  pres = bmp.readPressure() / 100.0F;
   alt = bmp.readAltitude(1013.25);
-  
 }
 
 // Return JSON String from sensor Readings
-String getJSONReadings(){
+String getJSONReadings()
+{
   readings["temperature"] = String(temp);
-  readings["humidity"] =  String(hum);
   readings["pressure"] = String(pres);
   readings["altitude"] = String(alt);
   String jsonString = JSON.stringify(readings);
@@ -77,118 +79,98 @@ String getJSONReadings(){
 }
 
 // Initialize SPIFFS
-void initSPIFFS() {
-  if (!SPIFFS.begin(true)) {
+void initSPIFFS()
+{
+  if (!SPIFFS.begin(true))
+  {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
-  else{
+  else
+  {
     Serial.println("SPIFFS mounted successfully");
   }
 }
 
-
-
-
 // ========================
 // Initialisation programme
 // ========================
-void setup() {
-  
+void setup()
+{
+
   // Initialisation du port série (pour l'envoi d'infos via le moniteur série de l'IDE Arduino)
   Serial.begin(115200);
-// Init BME280 senspr
+  // Init BME280 senspr
   initBMP();
-  
+
   // Init SPIFFS
   initSPIFFS();
 
-  
-   //************** connexion au wifi **************
+  //************** connexion au wifi **************
   WiFi.begin(ssid, password);
-	Serial.print("Tentative de connexion...");
-	
-	while(WiFi.status() != WL_CONNECTED)
-	{
-		Serial.print(".");
-		delay(100);
-	}
-	
-	Serial.println("\n");
-	Serial.println("Connexion etablie!");
-	Serial.print("Adresse IP: ");
-	Serial.println(WiFi.localIP());
+  Serial.print("Tentative de connexion...");
 
-  //************************  chargement des fichiers ****************** 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  while (WiFi.status() != WL_CONNECTED)
   {
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\n");
+  Serial.println("Connexion etablie!");
+  Serial.print("Adresse IP: ");
+  Serial.println(WiFi.localIP());
+
+  //************************  chargement des fichiers ******************
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html", "text/html"); });
 
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
-  {
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
+            { request->send(SPIFFS, "/style.css", "text/css"); });
 
   server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
-  {
-    request->send(SPIFFS, "/script.js", "text/javascript");
-  });
+            { request->send(SPIFFS, "/script.js", "text/javascript"); });
 
+  //******************** réponses aux requetes du client ************
+  // Affichage de la TEMPÉRATURE
+  server.on("/lireTemperature", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    String temps = String(bmp.readTemperature());
+    request->send(200, "text/plain", temps); });
 
-//******************** réponses aux requetes du client ************
-// Affichage de la TEMPÉRATURE
-  /*server.on("/lireTemperature", HTTP_GET, [](AsyncWebServerRequest *request)
-  {
-    String temp = String(bme.readTemperature());
-    request->send(200, "text/plain", temp);
-  });
-// Affichage du TAUX D'HUMIDITÉ
-  server.on("/LireHumidity", HTTP_GET, [](AsyncWebServerRequest *request)
-  {
-    String hum = String(bme.readHumidity());
-    request->send(200, "text/plain", hum);
-  });
- // Affichage de la PRESSION ATMOSPHÉRIQUE
+  // Affichage de la PRESSION ATMOSPHÉRIQUE
   server.on("/LirePresure", HTTP_GET, [](AsyncWebServerRequest *request)
-  {
-    String pres = String(bme.readPressure() / 100.0F);
-    request->send(200, "text/plain", pres);
-  });
+            {
+    String press = String(bmp.readPressure() / 100.0F);
+    request->send(200, "text/plain", press); });
   // Affichage de l'estimation d'ALTITUDE
   server.on("/lireAltitude", HTTP_GET, [](AsyncWebServerRequest *request)
-  {
-    String alt = String(bme.readAltitude(pressionAuNiveauDeLaMerEnHpa));
-    request->send(200, "text/plain", alt);
-  });
-*/
-// Request for the latest sensor readings
-    server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request){
+            {
+    String alts = String(bmp.readAltitude(1013.25));
+    request->send(200, "text/plain", alts); });
+
+  // Request for the latest sensor readings
+  server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
       getSensorReadings();
       String json = getJSONReadings();
-      request->send(200, "text/plain", json);
-      json = String();
-    });
-    
- 
+      request->send(200, "application/data", json);
+      json = String(); });
 
   server.begin();
   Serial.println("Serveur actif!");
-  
 }
-
 
 // ======================================
 // Boucle principale (boucle perpétuelle)
 // ======================================
-void loop() {
+void loop()
+{
 
   // Affichage de la TEMPÉRATURE
   Serial.print(F("Température = "));
   Serial.print(bmp.readTemperature());
   Serial.println(F(" °C"));
 
- 
-  
   // Affichage de la PRESSION ATMOSPHÉRIQUE
   Serial.print(F("Pression atmosphérique = "));
   Serial.print(bmp.readPressure());
@@ -199,10 +181,7 @@ void loop() {
   Serial.print(bmp.readAltitude(1013.25));
   Serial.println(F(" m"));
 
-
   // ... et on répète ce cycle à l'infini !
-  delay(2000);                // Avec x secondes d'attente, avant chaque rebouclage
-                                    // … et un saut de ligne pour faire joli ;)
-
+  delay(2000); // Avec x secondes d'attente, avant chaque rebouclage
+               // … et un saut de ligne pour faire joli ;)
 }
-
